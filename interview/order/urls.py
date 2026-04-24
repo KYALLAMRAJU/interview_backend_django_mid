a72d6f6d31bedde9e42aada029c9c4a7e3f0f151
KYALLAMRAJU <kalyanchakravarthy1992@gmail.com>
2026-04-23 20:55:49 -0500

    challenge-2: add DeactivateOrderView to set is_active state on an order

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+
+from interview.order.views import (
+    DeactivateOrderView,
+    OrderEmbargoDateView,
+    OrderListCreateView,
+    OrderTagListCreateView,
+    OrderTagsView,
+    OrdersByTagView,
+)
 
 
 urlpatterns = [
-    path("tags/", OrderTagListCreateView.as_view(), name="order-detail"),
+    path("tags/", OrderTagListCreateView.as_view(), name="order-tags-list"),
+    path("tags/<int:id>/orders/", OrdersByTagView.as_view(), name="orders-by-tag"),
+    path("embargo/", OrderEmbargoDateView.as_view(), name="order-embargo"),
+    path("<int:id>/tags/", OrderTagsView.as_view(), name="order-tags"),
+    path("<int:id>/deactivate/", DeactivateOrderView.as_view(), name="order-deactivate"),
     path("", OrderListCreateView.as_view(), name="order-list"),
 ]
